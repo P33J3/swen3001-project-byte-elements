a0c7e45ab6102d89ca62e7a3eeaba80c6302e28b
P33J3 <peterjonlawrence@gmail.com>
2024-10-15 19:13:12 -0500

    cleaned up comments

--- app/src/main/java/com/example/swen3001groupproject/ChildEventsActivity.kt
+++ app/src/main/java/com/example/swen3001groupproject/ChildEventsActivity.kt
@@ -16,17 +16,14 @@ class ChildEventsActivity : AppCompatActivity() {
 
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        // Dummy event data
-//        val eventList = ArrayList<ChildEvent>()
 
+        // Dummy event data
         eventList.add(ChildEvent(10, "Midsemester Exam", "January 10, 2024", "3:00PM", "SLT2, UWI Mona", true, false, false, true))
         eventList.add(ChildEvent(13, "Dad's Birthday Party", "January 13, 2024", "5:00PM", "Home", false, true, false, false))
         eventList.add(ChildEvent(23, "Track Meet", "January 23, 2024", "10:00AM", "National Stadium", false, true, false, true))
 
 
-        // Set up RecyclerView
-//        val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
-//        recyclerView.layoutManager = LinearLayoutManager(this)
+
         adapter = EventAdapter(eventList)
         recyclerView.adapter = adapter
         recyclerView.setHasFixedSize(true)
