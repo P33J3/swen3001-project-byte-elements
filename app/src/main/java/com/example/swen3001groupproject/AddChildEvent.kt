d3bbce06a9265eeafec0588000b07caa3e8ec598
Kale Williams <klaeboy.williams@gmail.com>
2024-10-17 17:07:43 -0500

    Logic of addEvent there but class in not parceble yet

--- app/src/main/java/com/example/swen3001groupproject/AddChildEvent.kt
+++ app/src/main/java/com/example/swen3001groupproject/AddChildEvent.kt
@@ -3,6 +3,8 @@ package com.example.swen3001groupproject
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,19 +12,45 @@ import androidx.core.view.WindowInsetsCompat
 
 class AddChildEvent : AppCompatActivity() {
     lateinit var add_button: Button
+    lateinit var indoorCheckbox: CheckBox
+    lateinit var outdoorCheckbox: CheckBox
+    lateinit var onlineCheckbox: CheckBox
+    lateinit var title: EditText
+    lateinit var date: EditText
+    lateinit var time: EditText
+    lateinit var location: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_child_event)
 
+
+
+        indoorCheckbox = findViewById(R.id.addCheckIndoors)
+        outdoorCheckbox = findViewById(R.id.addCheckOutdoors)
+        onlineCheckbox = findViewById(R.id.addCheckOnline)
+        title = findViewById(R.id.NewTitle)
+        date = findViewById(R.id.addEventDate)
+        time = findViewById(R.id.addEventTime)
+        location = findViewById(R.id.addEventLocation)
+
+        val eventTitle = title.text.toString()
+        val eventDate = date.text.toString()
+        val eventTime = time.text.toString()
+        val eventLocation = location.text.toString()
+        val indoor = indoorCheckbox.isChecked
+        val outdoor = outdoorCheckbox.isChecked
+        val online = onlineCheckbox.isChecked
+
+
         add_button = findViewById(R.id.button2)
         add_button.setOnClickListener {
 
-            //new_event = ChildEvent()
+            val newEvent = ChildEvent(17, eventTitle, eventDate, eventTime, eventLocation, indoor, outdoor, online, true)
 
             val intent = Intent(this, ChildEventsActivity::class.java)
-            intent.putExtra("new_event", new_event)
+            intent.putExtra("new_event", newEvent)
             startActivity(intent)
             this.finish()
         }
