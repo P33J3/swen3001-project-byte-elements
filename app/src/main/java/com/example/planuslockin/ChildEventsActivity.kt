fa93e723021adf79cea7d4c99035cc0439861ac3
Alexia <elizibrooks@gmail.com>
2024-11-26 05:13:43 -0500

    Edit function added(no db integration)

--- app/src/main/java/com/example/planuslockin/ChildEventsActivity.kt
+++ app/src/main/java/com/example/planuslockin/ChildEventsActivity.kt
@@ -8,11 +8,11 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.DataSnapshot
+/*import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ValueEventListener*/
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
